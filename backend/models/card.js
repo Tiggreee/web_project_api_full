517792e891c13250fa2eb26c75436c683405d054
TigreStack <notlikeiusedto@gmail.com>
2025-12-04 05:11:08 -0600

    fix close button style and card url validation

--- backend/models/card.js
+++ backend/models/card.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -12,7 +13,7 @@ const cardSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator(v) {
-        return /^https?:\/\/(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}([a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$/.test(v);
+        return validator.isURL(v);
       },
       message: 'Debe ser una URL válida',
     },
